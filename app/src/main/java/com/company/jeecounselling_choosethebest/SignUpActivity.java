3c5e6ce45b30f0724acb940a50581b52131c00e1
HASH-002 <koserhasan002@gmail.com>
2021-05-15 00:50:16 +0530

    SignUp and Login Activity

---
+++ app/src/main/java/com/company/jeecounselling_choosethebest/SignUpActivity.java
@@ -0,0 +1,125 @@
+package com.company.jeecounselling_choosethebest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText firstname;
+    private EditText lastname;
+    private EditText email;
+    private EditText password;
+    private Button register;
+    private TextView loginUser;
+
+    private DatabaseReference mRootRef;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        firstname = findViewById(R.id.firstname);
+        lastname = findViewById(R.id.lastname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        loginUser = findViewById(R.id.login_user);
+
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        loginUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtfirstname = firstname.getText().toString();
+                String txtlastname = lastname.getText().toString();
+                String txtEmail = email.getText().toString();
+                String txtPassword = password.getText().toString();
+
+                if (TextUtils.isEmpty(txtfirstname) || TextUtils.isEmpty(txtlastname)
+                        || TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)) {
+                    Toast.makeText(SignUpActivity.this, "Empty credentials!", Toast.LENGTH_SHORT).show();
+                } else if (txtfirstname.length() < 3) {
+                    Toast.makeText(SignUpActivity.this, "First name should have atleast 2 characters", Toast.LENGTH_SHORT).show();
+                } else if (txtlastname.length() < 3) {
+                    Toast.makeText(SignUpActivity.this, "Last name should have atleast 2 characters", Toast.LENGTH_SHORT).show();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(txtEmail).matches()) {
+                    email.setError("Enter valid email");
+                    email.requestFocus();
+                    return;
+                } else if (txtPassword.length() < 8) {
+                    Toast.makeText(SignUpActivity.this, "Password should have atleast 8 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser(txtfirstname, txtlastname, txtEmail, txtPassword);
+                }
+            }
+        });
+    }
+
+    private void registerUser(final String firstname, final String lastname, final String email, String password) {
+
+
+        mAuth.createUserWithEmailAndPassword(email , password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+
+                HashMap<String , Object> map = new HashMap<>();
+                map.put("firstname" , firstname);
+                map.put("lastname" , lastname);
+                map.put("email", email);
+                map.put("id" , mAuth.getCurrentUser().getUid());
+
+                mRootRef.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignUpActivity.this , MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) { // e message shows actual reason of the error
+                Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
